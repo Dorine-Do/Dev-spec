0c13756814db1de11580442a88173204906c00be
Baptiste Nautré <nautre.baptiste@hotmail.com>
2024-04-27 16:54:18 +0200

    DELETE produit

--- FRONT/app.js
+++ FRONT/app.js
@@ -202,6 +202,31 @@ app.post('/modifProduitID', async (req, res) => {
     }
 })
 
+app.post('/deleteProduit', async (req, res) => {
+    const delProduct = async () => {
+        const response = await fetch(`http://localhost:5000/products/${req.body.id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        });
+
+        if (response.ok) {
+            console.log(await response.text());
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    if(await delProduct()) {
+        res.redirect('/');
+    } else {
+        res.send('Erreur lors de la suppression du produit');
+    }
+})
+
 app.get('/test', (req, res) => {
    
     res.render('test', {cspNonce: req.nonce}); // Affichage page d'accueil avec filtre produit
